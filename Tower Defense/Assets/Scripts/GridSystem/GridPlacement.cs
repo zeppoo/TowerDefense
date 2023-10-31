86269e746c06cd91fc4718be3223f9815093cdc7
zeppoo <reinverhaag@gmail.com>
2023-10-31 12:55:40 +0100

    bullet werkt soort van

--- Tower Defense/Assets/Scripts/GridSystem/GridPlacement.cs
+++ Tower Defense/Assets/Scripts/GridSystem/GridPlacement.cs
@@ -33,5 +33,4 @@ public class GridPlacement : MonoBehaviour
             }
         }
     }
-    
 }
--- Tower Defense/Assets/Scripts/Objects/Bullet.cs
+++ Tower Defense/Assets/Scripts/Objects/Bullet.cs
@@ -7,28 +7,18 @@ public class Bullet : MonoBehaviour
 {
     [SerializeField] private int speed;
 
-    private void Start()
-    {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.right);
-        Debug.DrawRay(transform.position, transform.right, Color.white, 100);
-        if (hit.collider.tag == "Enemy")
-        {
-            Debug.Log("hit");
-        }
-    }
-
     private void Update()
     {
         transform.position += transform.right * Time.deltaTime * speed;
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("hit");
         if (collision.gameObject.CompareTag("Enemy"))
         {
             Debug.Log("hit");
-
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
         }
     }
 }
